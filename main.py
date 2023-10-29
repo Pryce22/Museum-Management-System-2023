6c65f139bc1c9e1fedbeaa80e13b10fa853f4954
Engel30 <angelo.plum@hotmail.it>
2023-10-29 16:55:49 +0100

    quando si preme su login/signup il software salva i dati nella var utente_attivo che si trova in vistaAccesso

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.uic.properties import QtWidgets
 
-from utente.view.VistaAccesso import Ui_VistaAccesso, mostra
+from utente.view.VistaAccesso import Ui_VistaAccesso, go_login_utente
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mostra()
+    go_login_utente()
     sys.exit(app.exec_())
 
---
+++ utente/model/Utente.py
@@ -0,0 +1,4 @@
+class Utente():
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
