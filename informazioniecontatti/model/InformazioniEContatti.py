fd38a7bd4bc21102fe3ac4e774c1d4564ff62452
MColletta02 <s1105265@studenti.univpm.it>
2023-11-01 18:02:44 +0100

    Vista InformazioniEContattiDipendente -> completo, manca il passaggio da vista home a vista informazioni e contatti

---
+++ informazioniecontatti/model/InformazioniEContatti.py
@@ -0,0 +1,18 @@
+class InformazioniEContatti:
+
+        def __init__ (self):
+            super(InformazioniEContatti, self).__init__()
+            self.informazioni = ""
+            self.contatti = ""
+
+        def aggiorna_informazioni(self, informazioni_aggiornate):
+            self.informazioni = informazioni_aggiornate
+
+        def aggiorna_contatti(self, contatti_aggiornati):
+            self.contatti = contatti_aggiornati
+
+        def get_informazioni(self):
+            return self.informazioni
+
+        def get_contatti(self):
+            return self.contatti
