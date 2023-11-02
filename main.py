a2bdf7c7c89c1135c6ff182c2c2500a5eb303fe7
Pryce22 <valerio.crocetti@gmail.com>
2023-11-02 20:28:13 +0100

    sistemato anche main (c'era un bene test)

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from utente.view.VistaAccesso import *
 from beni.model.Bene import Bene
 
 if __name__ == '__main__':
-    nuovo_bene = Bene("NomeBene", "Immagine", "Area", "Descrizione", "Stato", "StatoArea", "ID", "DataAggiunta")
-    controllore = ControlloreListaBeni()
-    controllore.inserisci_bene(nuovo_bene)
-
     app = QApplication(sys.argv)
     show_login_utente()
     sys.exit(app.exec_())
